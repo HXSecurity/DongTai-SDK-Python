a274a6a702df87b18fe86326bb2662f92839e337
covert_e <spenser@ygens.cn>
2021-12-23 21:42:03 +0800

    created setup file

---
+++ dongtai_sdk/base/__init__.py
@@ -0,0 +1,8 @@
+'''
+Author: 饕餮
+Date: 2021-12-23 21:34:39
+version: 
+LastEditors: 饕餮
+LastEditTime: 2021-12-23 21:34:39
+Description: file content
+'''
---
+++ setup.py
@@ -0,0 +1,31 @@
+'''
+Author: 饕餮
+Date: 2021-12-23 21:23:03
+version: 
+LastEditors: 饕餮
+LastEditTime: 2021-12-23 21:31:20
+Description: setup
+'''
+from __future__ import print_function
+from setuptools import setup,find_packages
+import sys
+
+setup(
+    name="dongtai_sdk",
+    version="0.0.1",
+    author="spensercai",
+    author_email="spensercai@gmail.com",
+    description="DongTai SDK",
+    license="MIT",
+    url="https://github.com/HXSecurity/DongTai-SDK-Python",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+
+    ],
+    install_requires=[
+
+    ],
+    zip_safe=True,
+
+)
